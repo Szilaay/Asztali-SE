0b78f1a470ec0bc359bfd65d328d33b4a9d350c8
Szilaay <86633348+Szilaay@users.noreply.github.com>
2025-03-19 08:18:35 +0100

    feat: default berek2020

---
+++ Berek2020/Berek2020/Dolgozok.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Berek2020
+{
+    internal class Dolgozok
+    {
+
+        string nev;
+        string neme;
+        string reszleg;
+        string belepes;
+        int ber;
+
+        public string Nev { get => nev; set => nev = value; }
+        public string Neme { get => neme; set => neme = value; }
+        public string Reszleg { get => reszleg; set => reszleg = value; }
+        public string Belepes { get => belepes; set => belepes = value; }
+        public int Ber { get => ber; set => ber = value; }
+
+        public Dolgozok(string sor)
+        {
+            string[] darabok = sor.Split(';');
+            nev = darabok[0];
+            neme = darabok[1];
+            reszleg = darabok[2];
+            belepes = darabok[3];
+            ber = int.Parse(darabok[4]);
+        }
+    }
+}
---
+++ Berek2020/Berek2020/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Berek2020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dolgozok> lista = new List<Dolgozok>();
+
+            FileStream fs = new FileStream("berek2020.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+
+            sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                lista.Add(new Dolgozok(sr.ReadLine()));
+            }
+
+            sr.Close();
+            fs.Close();
+
+            // 3. feladat
+            Console.WriteLine($"3. feladat: Dolgozók száma: {lista.Count()} fő");
+
+            // 4. feladat
+            double atlagfizu = Math.Round(lista.Average(x => x.Ber) / 1000, 1);
+            Console.WriteLine($"4. feladat: Bérek átlaga: {atlagfizu} eFt");
+
+            // 5. feladat
+            Console.Write("5. feladat: Kérem a részleg nevét: ");
+            string reszleg = Console.ReadLine();
+
+            List<Dolgozok> reszelges = new List<Dolgozok>();
+            foreach (var i in lista)
+            {
+                if (i.Reszleg == reszleg)
+                {
+                    reszelges.Add(i);
+                }
+            }
+            if(reszelges.Count == 0)
+            {
+                Console.WriteLine("6. feladat: A megadott részleg nem található");
+            }
+            else
+            {
+                int max = reszelges.Max(x => x.Ber);
+
+                Console.WriteLine($"6. feladat: A legtöbbet kereső dolgozó a megadott részlegen:");
+                Console.WriteLine($"\tNév {reszelges.First(x => x.Ber == max).Nev}");
+                Console.WriteLine($"\tNeme {reszelges.First(x => x.Ber == max).Neme}");
+                Console.WriteLine($"\tBelépés {reszelges.First(x => x.Ber == max).Belepes}");
+                Console.WriteLine($"\tBér: {reszelges.First(x => x.Ber == max).Ber.ToString("#,##0")} Forint");
+            }
+
+            // 7. feladat
+            Console.WriteLine("7. feladat: Statisztika");
+
+            Dictionary<string, int> statisztika = new Dictionary<string, int>();
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (statisztika.ContainsKey(lista[i].Reszleg))
+                {
+                    statisztika[lista[i].Reszleg]++;
+                }
+                else
+                {
+                    statisztika.Add(lista[i].Reszleg, 1);
+                }
+            }
+            foreach (var i in statisztika)
+            {
+                Console.WriteLine($"\t{i.Key} - {i.Value} fő");
+            }
+
+
+            Console.ReadKey(true);
+        }
+    }
+}

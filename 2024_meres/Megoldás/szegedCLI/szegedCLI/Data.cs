a0a519fd1190d3ab62b29bf2a3a7ff8d8526af88
Szilaay <86633348+Szilaay@users.noreply.github.com>
2025-03-20 11:18:51 +0100

    feat: added 2024_meres done

---
+++ 2024_meres/Megoldás/szegedCLI/szegedCLI/Data.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szegedCLI
+{
+    internal class Data
+    {
+        string jaratszam;
+        string jaratvegallomas;
+        string jaratmegallonev;
+        string jarattipus;
+
+        public string Jaratszam { get => jaratszam; set => jaratszam = value; }
+        public string Jaratvegallomas { get => jaratvegallomas; set => jaratvegallomas = value; }
+        public string Jaratmegallonev { get => jaratmegallonev; set => jaratmegallonev = value; }
+        public string Jarattipus { get => jarattipus; set => jarattipus = value; }
+
+        public Data(string sor)
+        {
+            string[] row = sor.Split(';');
+            jaratszam = row[0];
+            jaratvegallomas = row[1];
+            jaratmegallonev = row[2];
+            jarattipus = row[3];
+        }
+    }
+}
---
+++ 2024_meres/Megoldás/szegedCLI/szegedCLI/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace szegedCLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Data> lista = new List<Data>();
+
+            FileStream fs = new FileStream("szegedCLI_adatok.csv", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+
+            while(!sr.EndOfStream)
+            {
+                lista.Add(new Data(sr.ReadLine()));
+            }
+
+            sr.Close();
+            fs.Close();
+
+            //:...........:\\
+
+            //3. feladat
+            int megallok = lista.Count;
+            Console.WriteLine($"3. feladat: Szegeden {megallok} darab megálló található.");
+
+            //4. feladat
+            int trolimegallo = lista.Where(x => x.Jarattipus == "troli").Count();
+            Console.WriteLine($"4. feladat: Szegedi troli megállók száma: {trolimegallo}");
+
+            //5. feladat
+            //Határozza meg, hogy melyik járatnak hány megállója van!
+
+            Dictionary<string, int> jaratmegallo = new Dictionary<string, int>();
+            foreach (var item in lista)
+            {
+                if (jaratmegallo.ContainsKey(item.Jaratszam))
+                {
+                    jaratmegallo[item.Jaratszam]++;
+                }
+                else
+                {
+                    jaratmegallo.Add(item.Jaratszam, 1);
+                }
+            }
+
+            Console.WriteLine("5. feladat:");
+            foreach (var item in jaratmegallo)
+            {
+                Console.WriteLine($"\tJáratszám: {item.Key}, megállók száma: {item.Value} db");
+            }
+
+            //6. feladat
+            //Kérjen be a felhasználótól egy járatszámot, majd ellenőrizze, hogy az adott járat létezik-e (a
+            //keresést fejezze be, ha a járat létezik). Ha nincs ilyen járat, akkor írja ki a felhasználónak!
+
+            Console.WriteLine("6. feladat:");
+
+            Console.Write("\tAdjon meg egy járatszámot: ");
+            string jaratszam = Console.ReadLine();
+
+            if(!lista.Any(x => x.Jaratszam == jaratszam))
+            {
+                Console.WriteLine("\tNem létezik ilyen járat!");
+            }
+
+            //7. feladat
+
+            Console.WriteLine("7. feladat:");
+
+            List<string> keresett = new List<string>();
+
+            foreach(var item in lista)
+            {
+                if (item.Jaratszam == jaratszam)
+                {
+                    keresett.Add(item.Jaratmegallonev);
+                }
+            }
+
+            if(keresett.Count > 0)
+            {
+                int random = new Random().Next(0, keresett.Count);
+                Console.WriteLine($"\tA {jaratszam} járat egyik megállója {keresett[random]}");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey(true);
+        }
+    }
+}
